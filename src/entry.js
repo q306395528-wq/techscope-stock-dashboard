5927cc427b8b236b09863ebbbf8527d393ec8ed8
q306395528-wq <q306395528@gmail.com>
2026-08-07 22:22:48 +0800

    Report TechScope version 4.4.1

--- src/entry.js
+++ src/entry.js
@@ -17,7 +17,7 @@ export default {
       const response=await worker.fetch(request,env,ctx);
       if(!response.ok)return response;
       const data=await response.json();
-      return new Response(JSON.stringify({...data,version:'4.4.0'}),{status:200,headers:response.headers});
+      return new Response(JSON.stringify({...data,version:'4.4.1'}),{status:200,headers:response.headers});
     }
     return worker.fetch(request,env,ctx);
   },
