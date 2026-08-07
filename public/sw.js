cbabae750e5d4b05ce15271a0d4c7f62d3bf3484
q306395528-wq <q306395528@gmail.com>
2026-08-07 21:52:04 +0800

    Bump static cache for sidebar hotfix

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE = 'techscope-static-v4.2';
+const CACHE = 'techscope-static-v4.3';
 const STATIC_ASSETS = [
   '/', '/index.html', '/styles.css', '/home.css', '/sidebar-quotes.css', '/app.js', '/prelude.js', '/app-home.js', '/sidebar-quotes.js', '/app-core.js', '/app-data.js', '/app-charts.js',
   '/manifest.webmanifest', '/icon.svg'
